49dfa72a009280f825264f7462ac30820c19b913
Akihito Tamagawa <tama3bb@gmail.com>
2024-03-26 03:03:29 +0000

    gpt-4 with secret key

--- Chatbot.py
+++ Chatbot.py
@@ -17,13 +17,12 @@ for msg in st.session_state.messages:
 
 if prompt := st.chat_input():
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+        openai_api_key = st.secrets['OPENAI_API_KEY']
 
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-4-turbo-preview", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
